8e82391f28f62ae3735072a92888325d93e90d49
Akshata <Akshata@LAPTOP-4CVBJOT7>
2023-07-11 10:41:35 +0530

    update

---
+++ BookCRUD/Book1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCRUD
+{
+    public class Book1
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string aname { get; set; }
+        public double price { get; set; }
+    }
+    public class BookCRUD1
+    {
+        List<Book1> books;
+        public BookCRUD1()
+        {
+            books = new List<Book1>();
+        }
+        public void AddBook(Book1 b)
+        {
+            books.Add(b);
+        }
+        public List<Book1> GetBooks()
+        {
+            return books;
+        }
+        public void UpdateBook(Book1 b)
+        {
+            foreach (Book1 book in books)
+            {
+                if (book.id == b.id)
+                    book.name = b.name;
+                book.price = b.price;
+                break;
+
+            }
+        }
+        public void DeleteBook(int id)
+        {
+            foreach (Book1 book in books)
+            {
+                if (book.id == id)
+                {
+                    books.Remove(book);
+                }
+                break;
+            }
+        }
+    }
+}
--- BookCRUD/Program.cs
+++ BookCRUD/Program.cs
@@ -6,73 +6,25 @@ using System.Threading.Tasks;
 
 namespace BookCRUD
 {
-    public class Book
-    {
-        static void Main(string[] args)
-        {
-
-        }
-    }
-}
+   
 public class Book
 {
-    public int id { get; set; }
-    public string name { get; set; }
-    public string aname { get; set; }
-    public double price { get; set; }
-}
-public class BookCRUD1
-{
-    List<Book> books;
-    public BookCRUD1()
-    {
-        books = new List<Book>();
-    }
-    public void AddBook(Book b)
-    {
-        books.Add(b);
-    }
-    public List<Book> GetBooks()
-    {
-        return books;
-    }
-    public void UpdateBook(Book b)
-    {
-        foreach (Book book in books)
-        {
-            if (book.id == b.id)
-                book.name = b.name;
-            book.price = b.price;
-            break;
-
-        }
-    }
-    public void DeleteBook(int id)
-    {
-        foreach (Book book in books)
-        {
-            if (book.id == id)
-            {
-                books.Remove(book);
-            }
-            break;
-        }
-    }
+   
     static void Main(string[] args)
     {
         BookCRUD1 crud = new BookCRUD1();
         int option = 0;
         do
         {
-            Console.WriteLine("1. Add Emplyoee details");
-            Console.WriteLine("2. Update Employee");
-            Console.WriteLine("3. Delete Employee");
-            Console.WriteLine("4.Display Employee Details");
+            Console.WriteLine("1. Add Book details");
+            Console.WriteLine("2. Update Book Detils");
+            Console.WriteLine("3. Delete Book Details");
+            Console.WriteLine("4.Display Book Details");
             int option1 = Convert.ToInt32(Console.ReadLine());
             switch (option1)
             {
                 case 1:
-                    Book b1 = new Book();
+                    Book1 b1 = new Book1();
                     Console.WriteLine("Enter id");
                     b1.id = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine("Enter name");
@@ -81,11 +33,11 @@ public class BookCRUD1
                     b1.aname = Console.ReadLine();
                     Console.WriteLine("Enter Price");
                     b1.price = Convert.ToDouble(Console.ReadLine());
-                    crud.books.Add(b1);
+                    crud.AddBook(b1);
 
                     break;
                 case 2:
-                    Book b2 = new Book();
+                    Book1 b2 = new Book1();
                     Console.WriteLine("Enter id");
                     b2.id = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine("Enter name");
@@ -94,16 +46,16 @@ public class BookCRUD1
                     b2.aname = Console.ReadLine();
                     Console.WriteLine("Enter Price");
                     b2.price = Convert.ToDouble(Console.ReadLine());
-                    crud.books.Add(b2);
+                    crud.AddBook(b2);
                     break;
                 case 3:
-                    Book b3 = new Book();
+                    Book1 b3 = new Book1();
                     Console.WriteLine("Enter id to remove");
                     int id = Convert.ToInt32(Console.ReadLine());
                     break;
                 case 4:
-                    List<Book> list = new List<Book>();
-                    foreach (Book item in list)
+                    List<Book1> list = new List<Book1>();
+                    foreach (Book1 item in list)
                     {
                         Console.WriteLine($"{item.id} {item.name} {item.aname} {item.price}");
 
@@ -118,5 +70,5 @@ public class BookCRUD1
 
         } while (option == 1);
     }
-
+}
 }
